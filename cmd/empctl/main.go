37620c54a8b5e470626ef4a0ccee0fd75c65ab29
Ben Marini <ben@remind101.com>
2015-07-17 16:11:46 -0700

    Add aws flags

--- cmd/empctl/main.go
+++ cmd/empctl/main.go
@@ -7,10 +7,19 @@ import (
 )
 
 var commonFlags = []cli.Flag{
-	{
-		Name:      "app",
-		ShortName: "a",
-		Usage:     "The name of the app",
+	cli.StringFlag{
+		Name:  "app, a",
+		Usage: "The name of the app",
+	},
+	cli.StringFlag{
+		Name:   "aws.id",
+		EnvVar: "AWS_ACCESS_KEY_ID",
+		Usage:  "AWS Access Key ID",
+	},
+	cli.StringFlag{
+		Name:   "aws.key",
+		EnvVar: "AWS_SECRET_ACCESS_KEY",
+		Usage:  "AWS Secret Access Key",
 	},
 }
 
@@ -19,7 +28,7 @@ var Commands = []cli.Command{
 		Name:   "elb-status",
 		Usage:  "Check the ELB status for an app",
 		Flags:  commonFlags,
-		Action: runServer,
+		Action: runELBStatus,
 	},
 }
 
@@ -30,4 +39,8 @@ func main() {
 	app.Commands = Commands
 
 	app.Run(os.Args)
+}
+
+func runELBStatus(c *cli.Context) {
+
 }
